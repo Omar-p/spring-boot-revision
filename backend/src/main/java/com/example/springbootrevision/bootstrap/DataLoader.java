def56da54f9ddccc73a2d347ea65dd4f037cb13c
Omar Shabaan <omar.plus.077@gmail.com>
2023-05-26 17:31:29 +0300

    load fake data

---
+++ backend/src/main/java/com/example/springbootrevision/bootstrap/DataLoader.java
@@ -0,0 +1,36 @@
+package com.example.springbootrevision.bootstrap;
+
+import com.example.springbootrevision.customer.Customer;
+import com.example.springbootrevision.customer.CustomerRepository;
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataLoader {
+
+  @Bean
+  CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+    return args -> {
+      var faker = new Faker();
+      Random random = new Random();
+      Name name = faker.name();
+      String firstName = name.firstName();
+      String lastName = name.lastName();
+      int age = random.nextInt(16, 99);
+      String email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@amigoscode.com";
+      Customer customer = new Customer(
+          firstName +  " " + lastName,
+          email,
+          age);
+      customerRepository.save(customer);
+
+    };
+  }
+}
