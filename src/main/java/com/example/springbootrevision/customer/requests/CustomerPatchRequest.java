2ddeca71578c6434fe595580b0beddc0a2d64036
Omar Shabaan <omar.plus.077@gmail.com>
2023-05-26 16:16:49 +0300

    remove unused imports.

--- src/main/java/com/example/springbootrevision/customer/requests/CustomerPatchRequest.java
+++ src/main/java/com/example/springbootrevision/customer/requests/CustomerPatchRequest.java
@@ -1,8 +1,6 @@
 package com.example.springbootrevision.customer.requests;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record CustomerPatchRequest(
